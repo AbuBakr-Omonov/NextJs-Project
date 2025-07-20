06eb28f6c05baa7cd91be8aa7954e9f24c104fa8
AbuBakr-Omonov <omonov0780@gmail.com>
2025-07-20 16:31:53 +0500

    Detail-Page

--- src/app/(layout)/[id]/page.tsx
+++ src/app/(layout)/[id]/page.tsx
@@ -1,12 +1,13 @@
 import UsersDeatilItem from '@/components/UsersPage/UsersDeatilItem'
 import React from 'react'
+
 interface Props{
   params:{
     id:string
   }
 }
 const UserDetail = async ({params} :Props) => {
-    const id = await params.id
+    const id = params.id
      console.log(id)
        const data = await fetch(`https://dummyjson.com/users/${id}`)
       const usersDetail = await data.json()
